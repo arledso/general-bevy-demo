1c6b54ca9bb85e9f162494be14259940255ecda4
arledso <121693910+arledso@users.noreply.github.com>
2023-01-04 21:50:33 -0300

    Started work on condition and added bounds
    non-fuctional

--- src/quadtree.rs
+++ src/quadtree.rs
@@ -3,12 +3,21 @@
 enum Tree<T> {
     Branch {
         depth: u32,
+        bounds: ((f32, f32), (f32, f32)),
         children: BranchChildren<T>,
     },
     Cluster {
         depth: u32,
+        bounds: ((f32, f32), (f32, f32)),
         leaves: Vec<T>,
     }
+}
+
+enum Quadrant {
+    NW,
+    NE,
+    SW,
+    SE
 }
 
 #[derive(Clone)]
@@ -20,56 +29,92 @@ struct BranchChildren<T>{
 } 
 
 impl<T> BranchChildren<T>{
-    fn new(depth: u32) -> BranchChildren<T> {
+    fn new(depth: u32, bounds: ((f32,f32),(f32,f32))) -> BranchChildren<T> {
+        let ((x_1,y_1),(x_2,y_2)) = bounds;
+        let (x_o,y_o) = ((x_1+x_2)/2.0, (y_1+y_2)/2.0); //both used to div into quadrants
+
+        //draw diagram for help understanding
         return BranchChildren {
-            nw: Box::new(Tree.new_cluster(depth, Vec::<T>::new())),
-            ne: Box::new(Tree.new_cluster(depth, Vec::<T>::new())),
-            sw: Box::new(Tree.new_cluster(depth, Vec::<T>::new())),
-            se: Box::new(Tree.new_cluster(depth, Vec::<T>::new())),
+            nw: Box::new(Tree::new_cluster(depth, ((x_1,y_o),(x_o,y_2)), Vec::<T>::new())),
+            ne: Box::new(Tree::new_cluster(depth, ((x_o,y_o),(x_2,y_2)), Vec::<T>::new())),
+            sw: Box::new(Tree::new_cluster(depth, ((x_1,y_1),(x_o,y_o)), Vec::<T>::new())),
+            se: Box::new(Tree::new_cluster(depth, ((x_o,y_1),(x_2,y_o)), Vec::<T>::new())),
         }
     }
+    //add way to iterate over quadrants
 }
 
 impl<T> Tree<T> {
 
-    fn new_cluster (depth: u32, leaves: Vec<T>) -> Tree<T>{
-        return Tree::Cluster {depth: depth, leaves: leaves}
+    fn new_cluster (depth: u32, bounds: ((f32,f32),(f32,f32)), leaves: Vec<T>) -> Tree<T>{
+        return Tree::Cluster {depth: depth, bounds: bounds, leaves: leaves}
     }
 
-    fn new_branch (depth: u32) -> Tree<T>{
+    fn new_branch (depth: u32, bounds: ((f32,f32),(f32,f32))) -> Tree<T>{
         return Tree::Branch {
             depth: depth,
-            children: BranchChildren::new(depth + 1)
+            bounds: bounds,
+            children: BranchChildren::new(depth + 1, bounds)
         }
     }
 
-    fn leaf_clusters<T>(definition: u32, leaves: T) -> Vec<Vec<T>>{
+    fn leaf_clusters<F>(
+        definition: u32,
+        leaves: Vec<T>,
+        condition: impl Fn(T) -> (Quadrant, T), //change to take in T and bounds/relevant info
+        bounds: ((f32, f32), (f32, f32))
+        )-> Vec<Vec<T>>
+        {
         let mut leaf_clusters: Vec<Vec<T>> = Vec::<Vec<T>>::new();
 
         let mut current_queue: Vec<Box<Tree<T>>> = Vec::<Box<Tree<T>>>::new();
         let mut upcoming_queue: Vec<Box<Tree<T>>> = Vec::<Box<Tree<T>>>::new();
         
-        let mut tree: Box<Tree<T>> = Box::new(Tree::new_cluster(0, leaves));
+        let mut tree: Box<Tree<T>> = Box::new(Tree::new_cluster(0, bounds, leaves));
         current_queue.push(tree);
 
         while current_queue.get(0).is_some() {
 
             for mut tree_node in current_queue {
-                if tree_node.depth == definition || tree_node.leaves <= 1 {
-                    leaf_clusters.push(tree_node.leaves);
+
+                let Tree::Cluster{
+                    depth: node_depth,
+                    bounds: node_bounds,
+                    leaves: node_leaves
+                } = *tree_node;
+
+                if node_depth == definition || node_leaves.len() <= 1 {
+                    leaf_clusters.push(node_leaves);
                     continue;
                 }
                 
-                let cluster_leaves: Vec<T> = tree_node.leaves.clone();
-                let current_depth: u32 = tree_node.depth.copy();
-                tree_node = Tree::new_branch(current_depth);
+                tree_node = Box::new(Tree::new_branch(node_depth, node_bounds));
+                let Tree::Branch {
+                    depth: _,
+                    bounds: _,
+                    children: node_children 
+                } = *tree_node;
                 
-                upcoming_queue.push(tree_node.nw);
-                upcoming_queue.push(tree_node.ne);
-                upcoming_queue.push(tree_node.sw);
-                upcoming_queue.push(tree_node.se);
+                upcoming_queue.push(node_children.nw);
+                upcoming_queue.push(node_children.ne);
+                upcoming_queue.push(node_children.sw);
+                upcoming_queue.push(node_children.se);
+
+                let Tree::Cluster { depth: _, bounds: _, leaves: nw_leaves} = *node_children.nw;
+                let Tree::Cluster { depth: _, bounds: _, leaves: ne_leaves} = *node_children.ne;
+                let Tree::Cluster { depth: _, bounds: _, leaves: sw_leaves} = *node_children.sw;
+                let Tree::Cluster { depth: _, bounds: _, leaves: se_leaves} = *node_children.se;
+
+                for leaf in node_leaves {
+                    match condition(leaf).0 {
+                        NW => nw_leaves.push(leaf), //copy cur vec to local var; let nw_node = new_cluster(vec.push(leaf))
+                    }
+                }
                 //add leaves to respective branches
+                //use condition()        
+                //add way to know quadrant bounds/position
             }
+
             current_queue = upcoming_queue;
             upcoming_queue = Vec::<Box<Tree<T>>>::new();
         }
